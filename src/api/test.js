1745bbd7a7f5da94e181da29a9d2ad0fc7825d09
TamHM1410 <hunhminhtam@gmail.com>
2025-01-23 17:42:17 +0700

    commited

--- src/api/test.js
+++ src/api/test.js
@@ -9,6 +9,8 @@ export const get_data_excel = async () => {
       responseType: "arraybuffer",
       headers: {
         "Access-Control-Allow-Origin": "*",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Referer': 'https://go.microsoft.com',
         Origin: "https://go.microsoft.com",
       },
     });
