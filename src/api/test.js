7a6d24f19d548535f755607ea2901e9bb1039736
TamHM1410 <hunhminhtam@gmail.com>
2025-01-25 06:38:33 +0700

    update

--- src/api/test.js
+++ src/api/test.js
@@ -2,19 +2,22 @@ import axiosClient from "@/utils/axiosClient";
 import axios from "axios";
 export const get_data_excel = async () => {
   try {
-    const corsProxy = "https://cors-proxy.htmldriven.com/";
+    const corsProxy = "https://cors-anywhere.herokuapp.com/";
     const url = "https://go.microsoft.com/fwlink/?LinkID=521962";
 
-    const response = await axios.get(`${corsProxy}${url}`, {
+    const response = await axios.get(corsProxy + url, {
       responseType: "arraybuffer",
       headers: {
-        'Accept': 'application/octet-stream'
-      }
+        "Access-Control-Allow-Origin": "*",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Referer': 'https://go.microsoft.com',
+        Origin: "https://go.microsoft.com",
+      },
     });
 
     return response;
   } catch (error) {
-    console.error("Download error:", error);
+    console.error("Error:", error);
     throw error;
   }
 };
