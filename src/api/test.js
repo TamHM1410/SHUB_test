45032c856706ca25e9fc862bf69413322eb6d73b
TamHM1410 <hunhminhtam@gmail.com>
2025-01-25 06:18:57 +0700

    update

--- src/api/test.js
+++ src/api/test.js
@@ -2,16 +2,14 @@ import axiosClient from "@/utils/axiosClient";
 import axios from "axios";
 export const get_data_excel = async () => {
   try {
-    const corsProxy = "https://cors-anywhere.herokuapp.com/";
+    const corsProxy = "https://cors-anywhere.onrender.com/";
     const url = "https://go.microsoft.com/fwlink/?LinkID=521962";
 
     const response = await axios.get(corsProxy + url, {
       responseType: "arraybuffer",
       headers: {
-        "Access-Control-Allow-Origin": "*",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Referer': 'https://go.microsoft.com',
-        Origin: "https://go.microsoft.com",
+        'Accept': 'application/octet-stream',
+        'User-Agent': 'Mozilla/5.0'
       },
     });
 
