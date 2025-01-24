609a12a7aef621b23b2e42e440384c6c5c908ff5
TamHM1410 <hunhminhtam@gmail.com>
2025-01-25 06:36:07 +0700

    handeler

--- src/api/test.js
+++ src/api/test.js
@@ -2,19 +2,19 @@ import axiosClient from "@/utils/axiosClient";
 import axios from "axios";
 export const get_data_excel = async () => {
   try {
+    const corsProxy = "https://cors-proxy.htmldriven.com/";
     const url = "https://go.microsoft.com/fwlink/?LinkID=521962";
 
-    const response = await axios.get(url, {
+    const response = await axios.get(`${corsProxy}${url}`, {
       responseType: "arraybuffer",
       headers: {
-        Accept: "application/octet-stream",
-        "User-Agent": "Mozilla/5.0",
-      },
+        'Accept': 'application/octet-stream'
+      }
     });
 
     return response;
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Download error:", error);
     throw error;
   }
 };
